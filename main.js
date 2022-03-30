62f435fd2f5b7aac26b303352cb1be1489fef389
kimzath25130 <6306021612104@fitm.kmutnb.ac.th>
2022-03-30 13:56:47 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Socks',
+
+            description: 'Awar fuzzy pair of socks.'
+        }
+    }
+})
